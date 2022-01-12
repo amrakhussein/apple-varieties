c00729fcef2ed1172947f8b1365bb605a443ad80
Amro Abdelkamel <amromoorie@gmail.com>
2022-01-13 00:06:04 +0200

    Fix:: data testing assets removed + project prepare

--- app/src/App.js
+++ app/src/App.js
@@ -1,41 +1,37 @@
-import 'react-slideshow-image/dist/styles.css';
-import { hueRotate } from 'tailwindcss/defaultTheme';
-import './App.css';
-import SlideShow from './components/SlideShow';
-import SlideShowContaienr from './components/SlideShowContaienr';
-import {useFetch} from './useFetch'
+import { useFetch } from './useFetch';
 
 function App() {
   // Simulate remote data fetch
   // for dev purposes, json file located in public dir evading error
-  const { dataState, loading, error } = useFetch('./appleInfo.json')
-  console.log('data', dataState)
+  const { dataState, loading, error } = useFetch('./appleInfo.json');
+  console.log('data', dataState);
   return (
     <>
-    
-
       {/* <Hero /> */}
-      <div className='bg-yellow-100 h-screen'>
-      <div className='flex flex-col '>
-        {/* <div className='shrink-0 h-56'></div> */}
+      <div className="bg-yellow-100 h-screen">
+        <div className="flex flex-col ">
+          {/* <div className='shrink-0 h-56'></div> */}
 
-      <heading className=' my-5 px-3  ' >
-        <h2 className=' mt-20 text-7xl font-bold text-slate-700'>
-          {dataState.mainTitle}
-
-        </h2>
-        <figure>
-          <figcaption>
-          <img src={`${process.env.PUBLIC_URL}images/Hero.jpg`} alt='apples served on plate'></img>
-          <p className='text-right text-slate-500 text-xl '>Different apples' varities and their uses</p>
-          </figcaption>
-        </figure>
-        <p className=' text-slate-600 text-5xl prose pt-5  '>
-          {dataState.entryContent}
-        </p>
-      </heading>
-
-      </div>
+          <heading className=" my-5 px-3  ">
+            <h2 className=" mt-20 text-7xl font-bold text-slate-700">
+              {dataState.mainTitle}
+            </h2>
+            <figure>
+              <figcaption>
+                <img
+                  src={`${process.env.PUBLIC_URL}images/Hero.jpg`}
+                  alt="apples served on plate"
+                ></img>
+                <p className="text-right text-slate-500 text-xl ">
+                  Different apples' varities and their uses
+                </p>
+              </figcaption>
+            </figure>
+            <p className=" text-slate-600 text-5xl prose pt-5  ">
+              {dataState.entryContent}
+            </p>
+          </heading>
+        </div>
       </div>
     </>
   );
