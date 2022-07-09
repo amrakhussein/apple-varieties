45f2fa2d3f535f2cf5ff16d23b67aa106e1bf1d6
Amro Abdelkamel <amromoorie@gmail.com>
2022-07-09 23:51:45 +0200

    Fix(padding)

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -1,13 +1,13 @@
 // Data used:: from
 // -> https://pocketchangegourmet.com/the-definitive-guide-to-apples-and-their-uses/'
 
-import Layout from './layout/TheLayout'
 import appleInfo from '../appleInfo.json'
+import Hero from '../images/Hero.jpg'
 import Footer from './layout/TheFooter'
 import Header from './layout/TheHeader'
+import Layout from './layout/TheLayout'
 import Logo from './layout/TheLogo'
 import Section from './Section'
-import Hero from '../images/Hero.jpg'
 
 function App() {
   const appleInfoHero = () => {
@@ -32,46 +32,46 @@ function App() {
   // console.log('foo::', foo);
   return (
     <Layout>
-    <Logo />
-      <div className="shrink-0 h-14"></div>
+      <Logo />
+      <div className='shrink-0 h-14'></div>
       <Header />
 
       {/* Hero image below lg screen */}
-      <figure className="w-full ... lg:hidden ">
+      <figure className='w-full ... lg:hidden '>
         <img
-          className="mt-8 rounded-tl-xl rounded-br-xl border-x-0 border-solid border-red-400 h-[26rem] w-full  object-cover object-top ... sm:h-96 sm:w-full sm:object-cover sm:object-center  ... md:h-[30rem]"
+          className='mt-8 rounded-tl-xl rounded-br-xl border-x-0 border-solid border-red-400 h-[26rem] w-full  object-cover object-top ... sm:h-96 sm:w-full sm:object-cover sm:object-center  ... md:h-[30rem]'
           src={Hero}
-          alt="apples served on plate"
-          />
+          alt='apples served on plate'
+        />
         <figcaption>
-          <p className="text-right text-slate-500 text-sm pr-3 ">
+          <p className='text-right text-slate-500 text-sm pr-3 '>
             Different apples' varieties and their uses
           </p>
         </figcaption>
       </figure>
-      <main className="px-3">
+      <main className='px-3'>
         {/* flex row starting from lg screen */}
         {/* Section contains Hero image for lg screen and above */}
         <Section />
 
         {/* *** */}
         {/* flex Column */}
-        <div>
+        <div className='pl-6'>
           {/* idx of map is used since it's static data: quick fix of unique elems keys */}
           {appleInfoHero().map((data, idx) => (
             <>
               <h3
                 key={idx}
-                className=" pb-2 d mt-10 text-3xl font-bold text-slate-700 ... sm:mt-10"
+                className=' pb-2 d mt-10 text-3xl font-bold text-slate-700 ... sm:mt-10'
               >
                 {data.title}
               </h3>
-              <ul className="border-solid border-red-300 border-x-2 border-y-0 border-r-0 pl-2 ... ">
+              <ul className='border-solid border-red-300 border-x-2 border-y-0 border-r-0 pl-2 ... '>
                 {data.content.map((lists, idx) =>
                   lists.map((li, idx) => (
                     <li
                       key={idx}
-                      className="list-inside pl-4 prose text-md ... sm:leading-normal sm:text-lg   "
+                      className='list-inside pl-4 prose text-md ... sm:leading-normal sm:text-lg   '
                     >
                       {li.replace(/\./g, '')}
                     </li>
